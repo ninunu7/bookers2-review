6d00c00ecc36c4a1534f1de20db97beac6443315
ninunu7 <pibf51@gmail.com>
2021-06-24 05:45:19 +0000

    [fix]user#edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,12 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update
+    redirect_to user_path(user.id)
   end
 end
